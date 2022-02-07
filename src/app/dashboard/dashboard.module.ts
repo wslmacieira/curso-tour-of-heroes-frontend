7aa3e3734b5675ed76b9fd70fdbbc480d8664d8f
wslmacieira <wslmacieira@outlook.com>
2022-02-07 14:10:16 -0300

    Feature Module

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [CommonModule, MaterialModule, FlexLayoutModule, DashboardRoutingModule],
+})
+export class DashboardModule {}
