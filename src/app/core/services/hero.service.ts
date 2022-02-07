1b9503e00572397a65f4f6e857b9db3ef8e63680
wslmacieira <wslmacieira@outlook.com>
2022-02-07 18:09:23 -0300

    Ajuste Simular um Backend

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { Hero } from '../models/hero.model';
 import { environment } from './../../../environments/environment';
 import { MessageService } from './message.service';
-
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root',
@@ -18,17 +18,28 @@ export class HeroService {
   ) {}
 
   public getHeroes(): Observable<Hero[]> {
-    return this.http
-      .get<Hero[]>(this.heroesUrl)
-      .pipe(tap((heroes) => this.log(`fetched ${heroes.length} hero(es)`)));
+    if (environment.production) {
+      return this.http
+        .get<Hero[]>(this.heroesUrl)
+        .pipe(tap((heroes) => this.log(`fetched ${heroes.length} hero(es)`)));
+    } else {
+      this.messageService.add('HeroService: fetched heroes');
+      return of(HEROES);
+    }
   }
 
   getHero(id: number): Observable<Hero> {
-    return this.http
-      .get<Hero>(`${this.heroesUrl}/${id}`)
-      .pipe(
-        tap((hero) => this.log(`fetched hero id=${id} and name=${hero.name}`))
-      );
+    if (environment.production) {
+      return this.http
+        .get<Hero>(`${this.heroesUrl}/${id}`)
+        .pipe(
+          tap((hero) => this.log(`fetched hero id=${id} and name=${hero.name}`))
+        );
+    } else {
+      const hero = HEROES.find(hero => hero.id === id)!;
+      this.messageService.add(`HeroService: fetched hero id=${id}`)
+      return of(hero);
+    }
   }
 
   private log(message: string): void {
