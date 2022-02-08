0355b6f9e534be4c78764f645150a1fb4ee2908d
wslmacieira <wslmacieira@outlook.com>
2022-02-07 23:23:17 -0300

    Loading

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
+import { LoadingComponent } from './components/loading/loading.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
 
-const COMPONENTS = [MessagesComponent, ToolbarComponent, PageNotFoundComponent];
+const COMPONENTS = [
+  MessagesComponent,
+  ToolbarComponent,
+  PageNotFoundComponent,
+  LoadingComponent,
+];
 
 const MODULES = [CommonModule, MaterialModule, FlexLayoutModule, RouterModule];
 
@@ -15,6 +23,13 @@ const MODULES = [CommonModule, MaterialModule, FlexLayoutModule, RouterModule];
   declarations: [COMPONENTS],
   imports: [MODULES],
   exports: [MODULES, COMPONENTS],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
---
+++ src/app/core/services/loading.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+
+  public loading$: Observable<boolean> = this.loadingSubject.asObservable();
+
+  public hide(): void {
+    this.loadingSubject.next(false);
+  }
+
+  public show(): void {
+    this.loadingSubject.next(true);
+  }
+}
