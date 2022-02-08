9936745635c8fedef7bc82a88c31e3de9ab0e4fe
wslmacieira <wslmacieira@outlook.com>
2022-02-08 13:29:37 -0300

    Reactive Forms

--- src/app/heroes/components/hero-detail/hero-detail.component.ts
+++ src/app/heroes/components/hero-detail/hero-detail.component.ts
@@ -1,5 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../../../core/models/hero.model';
 import { HeroService } from '../../../core/services/hero.service';
@@ -12,11 +14,17 @@ import { HeroService } from '../../../core/services/hero.service';
 export class HeroDetailComponent implements OnInit {
   public hero!: Hero;
   public isEditing!: boolean;
+  public form = this.fb.group({
+    id: [{ value: '', disabled: true }],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+  });
 
   constructor(
+    private fb: FormBuilder,
     private heroService: HeroService,
     private location: Location,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -25,30 +33,52 @@ export class HeroDetailComponent implements OnInit {
 
   public getHero(): void {
     const paramId = this.route.snapshot.paramMap.get('id');
-    if (paramId === 'new') {
-      this.isEditing = false;
-      this.hero = {name: ''} as Hero;
-    } else {
+    if (paramId !== 'new') {
       this.isEditing = true;
-      const id = Number(paramId);
-      this.heroService.getOne(id).subscribe((hero) => (this.hero = hero));
-    }
 
+      const id = Number(paramId);
+      this.heroService.getOne(id).subscribe((hero) => {
+        this.hero = hero;
+        this.form.controls['id'].setValue(hero.id);
+        this.form.controls['name'].setValue(hero.name);
+      });
+    }
   }
 
   public goBack(): void {
-    this.location.back()
-  }
-
-  public isFormValid(): boolean {
-    return !!this.hero.name.trim()
+    this.location.back();
   }
 
   public create(): void {
-    this.heroService.create(this.hero).subscribe(() => this.goBack());
+    const { valid, value } = this.form;
+    // console.log(JSON.stringify({valid, value},null, 2));
+    if (valid) {
+      const hero: Hero = {
+        name: value.name,
+      } as Hero;
+      this.heroService.create(hero).subscribe(() => this.goBack());
+    } else {
+      this.showErrorMsg();
+    }
   }
 
   public update(): void {
-    this.heroService.update(this.hero).subscribe(() => this.goBack());
+    const { valid, value } = this.form;
+    if (valid) {
+      const hero: Hero = {
+        id: this.hero.id,
+        name: value.name,
+      };
+      this.heroService.update(hero).subscribe(() => this.goBack());
+    } else {
+      this.showErrorMsg();
+    }
+  }
+
+  private showErrorMsg(): void {
+    this.snackBar.open('Please check the errors found.', 'OK', {
+      duration: 5000,
+      verticalPosition: 'top',
+    });
   }
 }
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -9,6 +9,12 @@ import { HeroesRoutingModule } from './heroes-routing.module';
 
 @NgModule({
   declarations: [HeroesComponent, HeroDetailComponent],
-  imports: [CommonModule, FormsModule, MaterialModule, FlexLayoutModule, HeroesRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    HeroesRoutingModule,
+  ],
 })
 export class HeroesModule {}
